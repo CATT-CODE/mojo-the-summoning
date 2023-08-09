a4a7cb63bf1073adf325916b9f7a7d6d6513a98d
CATT-CODE <65243972+CATT-CODE@users.noreply.github.com>
2023-08-09 13:57:04 -0400

    User class

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,1 +1,17 @@
-// create your User model here
+const { db, Model, DataTypes } = require('../db')
+
+class User extends Model {}
+
+User.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    username: {
+        type: DataTypes.STRING,
+    }
+}, {
+    sequelize: db,
+    modelName: 'User'
+});
