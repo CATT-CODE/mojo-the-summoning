d37b8030bdd5fa9c9457878eacedd50927a70b3b
CATT-CODE <65243972+CATT-CODE@users.noreply.github.com>
2023-08-09 14:19:04 -0400

    Attack class

--- server/src/models/Attack.js
+++ server/src/models/Attack.js
@@ -1,1 +1,27 @@
 const { db, Model, DataTypes } = require('../db')
+
+class Attack extends Model {}
+
+Attack.init({
+    
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    title: {
+        type: DataTypes.STRING,
+    },
+    mojoCost: {
+        type: DataTypes.INTEGER
+    },
+    staminaCost: {
+        type: DataTypes.INTEGER
+    }
+}, {
+    sequelize: db,
+    modelName: 'Attack'
+});
+
+module.exports = Attack;
+
