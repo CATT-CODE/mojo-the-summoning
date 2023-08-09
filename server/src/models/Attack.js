39b8fbcac4d59a8b018dab845d0b58c9f1f21f7b
CATT-CODE <65243972+CATT-CODE@users.noreply.github.com>
2023-08-09 14:08:02 -0400

    Card class

---
+++ server/src/models/Attack.js
@@ -0,0 +1,1 @@
+const { db, Model, DataTypes } = require('../db')
---
+++ server/src/models/Card.js
@@ -0,0 +1,30 @@
+const { db, Model, DataTypes } = require('../db')
+
+class Card extends Model {}
+
+Card.init({
+    
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+    },
+    mojo: {
+        type: DataTypes.INTEGER
+    },
+    stamina: {
+        type: DataTypes.INTEGER
+    },
+    imgUrl: {
+        type: DataTypes.STRING
+    }
+}, {
+    sequelize: db,
+    modelName: 'Card'
+});
+
+module.exports = Card;
+
