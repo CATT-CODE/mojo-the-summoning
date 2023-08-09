662a9095232a08d42084e81061ac2e4803b49eb9
CATT-CODE <65243972+CATT-CODE@users.noreply.github.com>
2023-08-09 12:44:40 -0400

    Database connection

--- server/src/db/config.js
+++ server/src/db/config.js
@@ -1,1 +1,10 @@
 // add your database connection here
+const { Sequelize, DataTypes, Model } = require('sequelize');
+
+const db = new Sequelize({
+  dialect: 'sqlite',
+  storage: path.join(__dirname, 'db.sqlite'),
+  logging: true
+});
+
+module.exports = { db, DataTypes, Model };
